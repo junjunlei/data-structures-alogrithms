b81cdb32a142154c8433d88c87fca3c7e7efbb4c
雷俊俊 <junjun.lei@hand-china.com>
2020-11-12 08:47:36 +0800

    [ADD]:用栈实现括号匹配及单词反转 2020-11-12

--- src/c/stack/StackTest.java
+++ src/c/stack/StackTest.java
@@ -1,5 +1,7 @@
 package c.stack;
 
+import java.util.Stack;
+
 /**
  * @author junjun.lei
  * @create 2020-11-10 11:16
@@ -12,5 +14,61 @@ public class StackTest {
         stackA.push(3);
         stackA.push(4);
         System.out.println(stackA.pop());
+
+        String s1 = "[]{}()";
+        String s2 = "[(})]{";
+        System.out.println(isValid(s1));
+        System.out.println(isValid(s2));
+
+        String s3 = "abcde";
+
+        System.out.println(reverseStr(s3));
+    }
+
+    /**
+     * 括号匹配  []{}() true  [(})]{ false
+     *
+     * @param s
+     * @return
+     */
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '[' || c == '{' || c == '(') {
+                stack.push(c);
+            } else if (c == '}') {
+                if (stack.pop() != '{') {
+                    return false;
+                }
+            } else if (c == ']') {
+                if (stack.pop() != '[') {
+                    return false;
+                }
+            } else if (c == ')') {
+                if (stack.pop() != '(') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    /**
+     * 单词反转
+     *
+     * @param s
+     * @return
+     */
+    public static String reverseStr(String s) {
+        Stack<Character> stack = new Stack<>();
+        String out = "";
+        for (int i = 0; i < s.length(); i++) {
+            stack.push(s.charAt(i));
+        }
+        while (!stack.isEmpty()) {
+            out += stack.pop();
+        }
+        return out;
     }
 }
